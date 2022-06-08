a25dcc89a4131ee66b83d869462c98ac4bd670ed
pengyw97 <877285030@qq.com>
2022-06-08 22:00:53 +0800

    chore: 增加oneOf，提升打包速度

--- webpack.config.js
+++ webpack.config.js
@@ -12,32 +12,36 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpe?g|gif)$/i,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024
+        oneOf: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'less-loader']
+          },
+          {
+            test: /\.(png|svg|jpg|jpe?g|gif)$/i,
+            type: 'asset',
+            parser: {
+              dataUrlCondition: {
+                maxSize: 10 * 1024
+              }
+            },
+            generator: {
+              filename: 'static/images/[hash:10][ext]'
+            }
+          },
+          {
+            test: /\.(ttf|woff2?)$/,
+            type: 'asset/resource',
+            generator: {
+              filename: 'static/front/[hash:10][ext]'
+            }
           }
-        },
-        generator: {
-          filename: 'static/images/[hash:10][ext]'
-        }
-      },
-      {
-        test: /\.(ttf|woff2?)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/front/[hash:10][ext]'
-        }
+        ]
       }
     ]
   },
