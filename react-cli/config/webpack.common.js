c3deedd0384fe271e2568ee369ecf92189f3b01c
pengyw97 <877285030@qq.com>
2022-06-16 15:36:27 +0800

    feat: 配置babel解析js

--- react-cli/config/webpack.common.js
+++ react-cli/config/webpack.common.js
@@ -1,9 +1,23 @@
 const path = require("path");
 
 module.exports = {
+  mode: 'development',
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "js/[name].js",
   },
+  module: {
+    rules: [
+     {
+      oneOf: [
+        {
+          test: /.js/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        }
+      ]
+     }
+    ]
+  }
 }
--- react-cli/src/index.js
+++ react-cli/src/index.js
@@ -1,1 +1,8 @@
 console.log('hello webpack')
+
+const a = {
+  a1: 1,
+  a2: 2
+}
+
+const b = {...a}
