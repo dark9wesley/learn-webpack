3f92eafadd83953e7b07ebf0e253d5d243533df6
pengyw97 <877285030@qq.com>
2022-06-16 16:18:54 +0800

    build: 配置devServer

---
+++ react-cli/config/webpack.dev.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "js/[name].js",
+  },
+  module: {
+    rules: [
+     {
+      oneOf: [
+        {
+          test: /.js/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        }
+      ]
+     }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React-Cli',
+      template: path.resolve(__dirname, '../pubilc/index.html'),
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    open: true,
+    port: 3000,
+    hot: true,
+  }
+}
