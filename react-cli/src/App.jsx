ff730c82263fdf6cd5f0b45053055117b24ab9c2
pengyw97 <877285030@qq.com>
2022-06-17 15:14:33 +0800

    build: 配置@babel/plugin-transform-runtime

--- react-cli/src/App.jsx
+++ react-cli/src/App.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react"
+import User from "./utils/common"
+import User2 from "./utils/common2"
+
 const App = () => {
+
+  useEffect(() => {
+    const min = new User('ming', 24)
+    console.log(min.getName())
+    console.log(min.getAge())
+
+    const min2 = new User2('ming', 24)
+    console.log(min2.getName())
+    console.log(min2.getAge())
+  }, [])
+
   return (
     <>
       <div>App</div>
---
+++ react-cli/src/utils/common2.js
@@ -0,0 +1,16 @@
+class User2 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age
+  }
+}
+
+export default User2
