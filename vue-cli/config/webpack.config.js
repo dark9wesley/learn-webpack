723f50e567cdfbe53b73321b2eb17dac2400b1af
pengyw97 <877285030@qq.com>
2022-06-19 23:54:16 +0800

    install html-webpack-plugin for webpack build

--- vue-cli/config/webpack.config.js
+++ vue-cli/config/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('node:path')
-
+const HtmlPlugin = require('html-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
@@ -9,4 +9,10 @@ module.exports = {
     path: isDev ? undefined : path.resolve(__dirname, '../dist'),
     filename: 'js/[name].[contenthash:10].js'
   },
+  plugins: [
+    new HtmlPlugin({
+      template: path.resolve(__dirname, '../public/index.html'),
+      title: 'vue-cli-demo',
+    }),
+  ]
 }
