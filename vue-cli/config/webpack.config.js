8f6eba90d674c131d229ec7868cdf19264132c8a
pengyw97 <877285030@qq.com>
2022-06-19 23:46:17 +0800

    build: set the webpack file according to the environment variable

--- vue-cli/config/webpack.config.js
+++ vue-cli/config/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('node:path')
 
+const isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
+  mode: isDev ? 'development' : 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: isDev ? undefined : path.resolve(__dirname, '../dist'),
     filename: 'js/[name].[contenthash:10].js'
   },
-  mode: 'development',
 }
