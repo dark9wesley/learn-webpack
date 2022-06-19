96812807a2ed9aef68cea9c7e7b4bc97e07a4b07
pengyw97 <877285030@qq.com>
2022-06-20 06:04:18 +0800

    build: set webpack config to parse vue file

--- vue-cli/config/webpack.config.js
+++ vue-cli/config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('node:path')
 const HtmlPlugin = require('html-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development'
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
@@ -9,14 +10,23 @@ module.exports = {
     path: isDev ? undefined : path.resolve(__dirname, '../dist'),
     filename: 'js/[name].[contenthash:10].js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+    ],
+  },
   plugins: [
     new HtmlPlugin({
       template: path.resolve(__dirname, '../public/index.html'),
       title: 'vue-cli-demo',
     }),
+    new VueLoaderPlugin(),
   ],
   devServer: {
-    port: 3000,
+    port: 4000,
     open: true,
     hot: true,
     historyApiFallback: true,
